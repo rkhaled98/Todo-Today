d980b766ded2bbd82bb6b9a3507ca8dd76efc7e2
Rafi Khaled <rkhaled@vassar.edu>
2019-03-14 23:48:12 -0400

    adding resolutions field

--- client/App.jsx
+++ client/App.jsx
@@ -2,12 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import './index.html'
+import {Mongo} from 'meteor/mongo'
 
+Resolutions = new Mongo.Collection("resolutions")
 
 export default class App extends React.Component{
+
+    addResolution(event){
+        event.preventDefault();
+        var text = this.refs.resolution.value;
+        console.log(text)
+    }
+
     render(){
         return (
-            <h1>Hello World </h1>
+            <div>
+            <h1>My resolutions</h1>
+            <form className="new-resolution" onSubmit={this.addResolution.bind(this)}>
+                <input
+                    type="text"
+                    ref="resolution"
+                    placeholder="Go to the gym..."
+                />
+            </form>
+            </div>
         )
     }
 }
